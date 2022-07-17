cd8370c0de5f82635e142fc628d5f52b235808bf
yakubgitty <96181544+yakubgitty@users.noreply.github.com>
2022-07-17 14:16:30 +0530

    Update creatersgrp.tf

--- resgrp/creatersgrp.tf
+++ resgrp/creatersgrp.tf
@@ -6,14 +6,21 @@ terraform {
     }
   }
 }
-
+terraform {
+  backend "azurerm" {
+/*     resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate" */
+  }
+}
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id = "c6b3494e-5f7c-4336-9ff6-16978f844f83"
+/*   subscription_id = "c6b3494e-5f7c-4336-9ff6-16978f844f83"
   client_id       = "f4a4b4cc-102d-4827-86f6-9b3f5fc080d1"
   client_secret   = "ytS8Q~CyC6_bbLzT7Xw.Y6QxcqPAwIZGycsYLcNw"
-  tenant_id       = "f96fe015-6d5f-4536-a865-48dc3befedce"
+  tenant_id       = "f96fe015-6d5f-4536-a865-48dc3befedce" */
 }
 
 resource "azurerm_resource_group" "example19juneblock" {
